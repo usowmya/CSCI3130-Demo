47d7691944d2ee995e8e054b1703a1e35115cb69
usowmya <35606837+usowmya@users.noreply.github.com>
2018-01-22 19:49:06 -0400

    Assignment-1
    question 3

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText input;
+    private TextView text;
+    private Button copy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = findViewById(R.id.editText);
+        copy = findViewById(R.id.button);
+        text = findViewById(R.id.textView);
+
+        copy.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String content = input.getText().toString();
+                text.setText(content);
+
+
+            }
+
+
+        });
+
+
     }
 }
